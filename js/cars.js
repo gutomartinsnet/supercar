d8462f28705b3e25aa79c3c74de9b97615ddc48a
Chris Barber <chrisbarber86@gmail.com>
2011-10-31 22:31:30 +0000

    Initial Commit

---
+++ js/cars.js
@@ -0,0 +1,60 @@
+/* Website: Supercar Top Trumps
+Developer: Chris Barber
+Version: 1.0
+Date: 31/10/2011
+File: cars.js */
+
+/*  Copyright 2011 Chris Barber (email: chris@chris-barber.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  150
+*/
+
+// This file contains nothing but an array of cars
+
+// Set up Cars:
+var cars = new Array();
+
+// Image Ref, Car Name, Car Country Ref, Top Speed, 0-60, Power, Engine Size cc, Weight Kgs
+cars[0] 	= new Array("005", "AC Cobra", "150", 160, 4.2, 485, 6997, 1148);
+cars[1] 	= new Array("010", "Aston Martin DB9", "100", 186, 4.9, 450, 5935, 1750);
+cars[2] 	= new Array("015", "Bentley Continental GT", "100", 196, 4.8, 552, 5998, 2385);
+cars[3] 	= new Array("020", "BMW M3 CSL", "110", 155, 4.6, 360, 3246, 1460);
+cars[4] 	= new Array("025", "Bugatti Veyron", "120", 252, 2.8, 987, 7993, 1600);
+cars[5] 	= new Array("030", "Caterham R500", "100", 145, 3.5, 230, 1796, 460);
+cars[6] 	= new Array("035", "Chevrolet Corvette Z06", "150", 172, 4.3, 405, 5665, 1413);
+cars[7] 	= new Array("040", "Dodge Viper SRT-10", "150", 189, 4.0, 500, 8285, 1542);
+cars[8] 	= new Array("045", "Ferrari 360 Stradale", "120", 188, 4.0, 425, 3586, 1280);
+cars[9] 	= new Array("050", "Ferrari Enzo", "120", 218, 3.4, 650, 5998, 1365);
+cars[10] 	= new Array("055", "Ferrari F40", "120", 201, 3.9, 478, 2936, 1100);
+cars[11]	= new Array("060", "Ford GT", "150", 200, 3.8, 500, 5409, 1519);
+cars[12]	= new Array("065", "Honda NSX Type R", "130", 168, 4.6, 276, 3179, 1270);
+cars[13] 	= new Array("070", "Jaguar XJ220", "100", 217, 3.6, 542, 3500, 1456);
+cars[14] 	= new Array("075", "Koenigsegg CC8S", "140", 235, 4.2, 655, 4700, 1275);
+cars[15] 	= new Array("080", "Lamborghini Countach", "120", 185, 4.8, 455, 5167, 1447);
+cars[16] 	= new Array("085", "Lamborghini Gallardo", "120", 192, 4.1, 493, 4961, 1520);
+cars[17] 	= new Array("090", "Lotus Esprit Sport 350", "100", 175, 4.3, 350, 3506, 1300);
+cars[18] 	= new Array("095", "Maserati MC12", "120", 205, 3.7, 622, 5998, 1335);
+cars[19] 	= new Array("100", "McLaren F1", "100", 240, 3.2, 627, 6064, 1137);
+cars[20]	= new Array("105", "Mercedes SL55 AMG", "110", 155, 4.6, 493, 5439, 1880);
+cars[21]	= new Array("110", "Mercedes SLR McLaren", "110", 207, 3.7, 617, 5439, 1693);
+cars[22]	= new Array("115", "Mitsubishi Evo VIII", "130", 148, 4.9, 276, 1997, 1410);
+cars[23]	= new Array("120", "Noble M12 GTO3R", "100", 170, 3.9, 352, 2968, 1080);
+cars[24]	= new Array("125", "Pagani Zonda C12S", "120", 215, 3.8, 542, 7010, 1250);
+cars[25] 	= new Array("130", "Porsche 959", "110", 197, 3.9, 450, 2851, 1450);
+cars[26] 	= new Array("135", "Porsche GT3 RS", "110", 190, 4.3, 381, 3600, 1350);
+cars[27] 	= new Array("140", "Porsche Carrera GT", "110", 205, 3.7, 604, 5733, 1380);
+cars[28] 	= new Array("145", "Radical SR3 Turbo", "100", 160, 3.2, 320, 1500, 525);
+cars[29] 	= new Array("150", "Saleen S7", "150", 239, 3.3, 550, 7008, 1247);
+cars[30] 	= new Array("155", "Subaru Impreza WR1", "130", 155, 4.5, 316, 1994, 1470);
+cars[31] 	= new Array("160", "TVR Tuscan S", "100", 185, 3.8, 400, 3996, 1100);
---
+++ js/flags.js
@@ -0,0 +1,34 @@
+/* Website: Supercar Top Trumps
+Developer: Chris Barber
+Version: 1.0
+Date: 31/10/2011
+File: flags.js */
+
+/*  Copyright 2011 Chris Barber (email: chris@chris-barber.co.uk)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// This file contains nothing but an array of flags
+
+// Set up Flags:
+var flags 	= new Array();
+
+// Image Ref, Country Name
+flags[0] 	= new Array("100", "GB");
+flags[1] 	= new Array("110", "GERMANY");
+flags[2] 	= new Array("120", "ITALY");
+flags[3] 	= new Array("130", "JAPAN");
+flags[4] 	= new Array("140", "SWEDEN");
+flags[5] 	= new Array("150", "USA");
