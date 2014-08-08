75b98a1dcfac4f9304e68e68e565d3ec756153af
Chris Barber <chris.barber@sage.com>
2014-08-08 23:08:33 +0100

    House keeping

--- js/flags.js
+++ js/flags.js
@@ -1,24 +1,19 @@
-/* Website: Supercar Top Trumps
-   Developer: Chris Barber
-   Version: 1.0
-   Date: 31/10/2011
-   File: flags.js */
+/**
+ * Copyright 2014 Chris Barber (email: chris@chris-barber.co.uk)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 2, as
+ * published by the Free Software Foundation.
 
-/*  Copyright 2011 Chris Barber (email: chris@chris-barber.co.uk)
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as
-    published by the Free Software Foundation.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-    */
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
 // This file contains nothing but an array of flags
 // Set up Flags:
